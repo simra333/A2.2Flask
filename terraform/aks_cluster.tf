d74fb4809ddc56a7840f83209dc095b660ca699b
Simra Abid <simra_2000@hotmail.co.uk>
2025-11-07 13:10:19 +0000

    changed aks vm image

--- terraform/aks_cluster.tf
+++ terraform/aks_cluster.tf
@@ -1,13 +1,13 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = pythonapp-aks-cluster-sa
-  location            = UKSouth
+  name                = "pythonapp-aks-cluster-sa"
+  location            = "UKSouth"
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "pythonappaks"
 
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2ms"
   }
 
   identity {
