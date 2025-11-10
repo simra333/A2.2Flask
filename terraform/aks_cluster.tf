0bbb00086c904b5800b5eb86de4a1c0ce6935f7e
Simra Abid <simra_2000@hotmail.co.uk>
2025-11-10 09:34:23 +0000

    added files to gitignore

--- terraform/aks_cluster.tf
+++ terraform/aks_cluster.tf
@@ -19,11 +19,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
-
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
--- terraform/terraform.tfvars
+++
@@ -1,1 +0,0 @@
-subscription_id = "7d7c4617-06a8-4d8b-8dfc-d31d8d7e18eb"
